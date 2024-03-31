5fded96d47fe19e7ae25c7289132410bddd3db72
noisfischer <noisfischer@gmail.com>
2024-03-31 10:37:06 -0400

    MultiplayerSessionsSubsystem Init
    Created multiplayer sessions subsystem of type game instance subsystem. This subsystem will be initialized once the game instance is initialized. Stored a pointer to the IOnlineSession interface to use later

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -0,0 +1,11 @@
+#include "MultiplayerSessionsSubsystem.h"
+#include "OnlineSubsystem.h"
+
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+{
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if(Subsystem)
+	{
+		SessionInterface = Subsystem->GetSessionInterface();
+	}
+}
---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "MultiplayerSessionsSubsystem.generated.h"
+
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UMultiplayerSessionsSubsystem();
+	
+protected:
+	
+private:
+	IOnlineSessionPtr SessionInterface;
+};
